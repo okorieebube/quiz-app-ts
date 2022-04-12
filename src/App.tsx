36c1dbfa31760ce27c3fca5d2d16567130259be5
Ebube <okorieebube1@gmail.com>
2022-04-12 06:51:01 +0100

    setting up quiz project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { User } from './interfaces';
+
+
 
 function App() {
+  const [user, setUser] = useState<User | null>(null);
+  let fetchUser = () => {
+    setUser({
+      name: "Mitchel",
+      age: 23,
+      country: "Nigeria",
+      admin: false
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello World!</h1>
+      <button onClick={fetchUser}>Click me</button>
+      {user && <p>Welcome, {user.name}</p>}
+    </>
+
   );
 }
 
